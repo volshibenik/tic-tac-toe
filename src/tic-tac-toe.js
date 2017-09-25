90bace275b60508cf0e043d99e819c1b6071dff7
Sergey S <volshibenik@gmail.com>
2017-09-25 16:07:07 +0300

    work in progress, also added .gitignore, bc it somehow missed

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,14 +1,35 @@
 class TicTacToe {
     constructor() {
-
+			this.field = [[null, null, null],
+										[null, null, null],
+										[null, null, null]]
     }
 
     getCurrentPlayerSymbol() {
-
+			var arr = this.field.reduce(function(a,b) {
+				return a.concat(b);
+			});
+			var arrX = arr.filter(function(e){
+				return e ==='x';
+			});
+			var arrO = arr.filter(function(e){
+				return e ==='o';
+			});
+			if (arrX.length <= arrO.length) {
+				return 'x'
+			} else return 'o';
     }
 
     nextTurn(rowIndex, columnIndex) {
-
+			if (this.getFieldValue(rowIndex, columnIndex) === null) {
+				if (this.getCurrentPlayerSymbol() === 'x') {
+					this.field[rowIndex][columnIndex] = 'x';
+				} else {
+					this.field[rowIndex][columnIndex] = 'o'
+				}
+			}
+			
+			
     }
 
     isFinished() {
@@ -28,7 +49,7 @@ class TicTacToe {
     }
 
     getFieldValue(rowIndex, colIndex) {
-
+			return this.field[rowIndex][colIndex];
     }
 }
 
